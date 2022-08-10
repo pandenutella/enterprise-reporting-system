4756bd9d4137862eb583483bf226e88af1142937
johnemmanuelb <johnemmanuel.bacalla@gmail.com>
2022-08-11 01:29:13 +0800

    Create get all api for reports

--- pages/api/reports/index.js
+++ pages/api/reports/index.js
@@ -3,8 +3,8 @@ import dbPromise from "../../../lib/mongodb";
 const handler = async (_, res) => {
   const db = await dbPromise;
 
-  const reportGroups = await db.collection("reportGroups").find({}).toArray();
-  res.json(reportGroups);
+  const reports = await db.collection("reports").find({}).toArray();
+  res.json(reports);
 };
 
 export default handler;
